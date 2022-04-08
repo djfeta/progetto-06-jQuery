2d04161ef141d515f0dccf668d96ea887c910a1c
djfeta <kiriazisgiovanni@gmail.com>
2022-04-08 16:40:36 +0200

    nuovo commit

--- progetto-memory-game-solo-indicazioni/script.js
+++ progetto-memory-game-solo-indicazioni/script.js
@@ -1,28 +1,109 @@
-const mieImg = ["arrabbiato", "bello", "piangere", "ridere", "amare", "amare1", "spavento", "shock", "arrabbiato", "bello",
-    "piangere", "ridere", "amare", "amare1", "spavento", "shock"];
-// creo una variabile che mi contiene le immagini cliccate
+var scatolaAperta = "";
+var immagineAperta = "";
+var contatore = 0;
+var imgTrovata = 0;
 
-// quando il documento è pronto...vado a selezionare casualmente una immagine dalla cartella.
+var radice = "#scatolaimmagini";
 
-    // creo una variabile "images" che contiene il selettore della classe "images". 
-    // così ho un oggetto jQuery e metto gli elementi corrispondenti nell'oggetto jQuery.
+var immagini = [
+    'img/amare.png',
+    'img/amare1.png',
+    'img/arrabbiato.png',
+    'img/bello.png',
+    'img/piangere.png',
+    'img/ridere.png',
+    'img/shock.png',
+    'img/spavento.png'
+];
 
-    // creo un ciclo for sull'oggetto creato per ottenere poi un'immagine random.
+function RandomFunction(MaxValue, MinValue) {
+    return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
+}
 
-        // prendo a caso un elemento dalla mia lista.
+function mescola() {
+    var ImgTutte = $(radice).children();
+    var ImgQst = $(radice + " div:first-child");
+    var ImgArr = new Array();
 
-        // vado a prendere il file localizzato nella directory img e creo il tag html e lo inserisco nella pagina.
-        //images.eq(e).html("<img id='" + e + "' src='images/" + randomImg + ".png' width='130' height='130' />");
-  
- // creo la funzione principale "mostraImg"
+    for (var i = 0; i < ImgTutte.length; i++) {
+        ImgArr[i] = $("#" + ImgQst.attr("id") + " img").attr("src");
+        ImgQst = ImgQst.next();
+    }
 
-    // creo l'oggetto jQuery per i clicks e lo chiamo "tuttiIClick" e prendo il valore e poi incremento.
-    
-    // ora visualizzo l'emoji (l'immagine) a due a due se sono diverse le nascondo altrimenti le lascio visibili.
-    
-        // se non sono due visualizzo l'emoji e la inserisco in "clickImgs".
-        
-            // se sono uguali azzero la mia lista.
-            
-                // se sono diverse nascondo le due immagini.
-                
+    ImgQst = $(radice + " div:first-child");
+
+    for (var z = 0; z < ImgTutte.length; z++) {
+        var RandomNumber = RandomFunction(0, ImgArr.length - 1);
+
+        $("#" + ImgQst.attr("id") + " img").attr("src", ImgArr[RandomNumber]);
+        ImgArr.splice(RandomNumber, 1);
+        ImgQst = ImgQst.next();
+    }
+}
+
+function resetta() {
+    mescola();
+    $(radice + " div img").hide();
+    $(radice + " div").css("visibility", "visible");
+    contatore = 0;
+    $("#success").remove();
+    $("#contatore").html("" + contatore);
+    scatolaAperta = "";
+    immagineAperta = "";
+    imgTrovata = 0;
+    return false;
+}
+
+function OpenCard() {
+    var id = $(this).attr("id");
+
+    if ($("#" + id + " img").is(":hidden")) {
+        $(radice + " div").unbind("click", OpenCard);
+
+        $("#" + id + " img").slideDown('fast');
+
+        if (immagineAperta == "") {
+            scatolaAperta = id;
+            immagineAperta = $("#" + id + " img").attr("src");
+            setTimeout(function () {
+                $(radice + " div").bind("click", OpenCard)
+            }, 300);
+        } else {
+            CurrentOpened = $("#" + id + " img").attr("src");
+            if (immagineAperta != CurrentOpened) {
+                setTimeout(function () {
+                    $("#" + id + " img").slideUp('fast');
+                    $("#" + scatolaAperta + " img").slideUp('fast');
+                    scatolaAperta = "";
+                    immagineAperta = "";
+                }, 400);
+            } else {
+                $("#" + id + " img").parent().css("visibility", "hidden");
+                $("#" + scatolaAperta + " img").parent().css("visibility", "hidden");
+                imgTrovata++;
+                scatolaAperta = "";
+                immagineAperta = "";
+            }
+            setTimeout(function () {
+                $(radice + " div").bind("click", OpenCard)
+            }, 400);
+        }
+        contatore++;
+        $("#contatore").html("" + contatore);
+
+        if (imgTrovata == immagini.length) {
+            $("#contatore").prepend('<span id="success">hai risolto il memory in </span>');
+        }
+    }
+}
+
+$(function () {
+
+    for (var y = 1; y < 3; y++) {
+        $.each(immagini, function (i, val) {
+            $(radice).append("<div id=card" + y + i + "><img src=" + val + " />");
+        });
+    }
+    $(radice + " div").click(OpenCard);
+    mescola();
+});
